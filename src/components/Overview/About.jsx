14e19f0b2e8367de15032f1d73b15a163145581e
jpanay <jasonpanay@gmail.com>
2024-08-19 01:06:51 -0400

    updated about

--- src/components/Overview/About.jsx
+++ src/components/Overview/About.jsx
@@ -1,16 +1,17 @@
 const About = () => {
   return (
     <div className="text-center max-w-2xl">
-      <div className="space-y-2">
+      <div className="space-y-2 px-4">
         <p>Hey I'm Jason, welcome to my site!</p>
         <p>
-          I'm a lifelong coder who has spent the past 4 years doing full stack
-          development for various start-ups and companies throughout the US.
+          As a lifelong coder, I've honed my skills over the past four years
+          doing full-stack development for various start-ups and companies in my
+          hometown of NYC and across the US.
         </p>
         <p>
-          When I'm not coding or falling down the YouTube rabbit hole I like to
-          spend time rock climbing, exploring the city, and making new
-          connections.
+          When I'm not coding or falling down the YouTube rabbit hole, I like to
+          spend my time rock climbing, exploring the city, and learning new
+          skills.
         </p>
       </div>
     </div>
