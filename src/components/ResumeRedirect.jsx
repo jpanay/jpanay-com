2f650fb0629806a834025dffc269bb33fbbc56e2
Jason Panay <70326429+jpanay@users.noreply.github.com>
2025-03-08 10:57:16 -0500

    update resume link

--- src/components/ResumeRedirect.jsx
+++ src/components/ResumeRedirect.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const ResumeRedirect = () => {
   const resumeLink =
-    "https://drive.google.com/file/d/11UQHZwzicK_Lhmk524E4gbI2zCKpH2zB/view?usp=sharing";
+    "https://drive.google.com/file/d/15Mxxr3pyzAbCznCVwuCOk_IM6G4Hocq7/view?usp=sharing";
 
   useEffect(() => (window.location.href = resumeLink), []);
 
